9f4332b7282956bb11a0d3d031d73152fc2f9c29
zu-min <okka.0928@gmail.com>
2021-07-27 18:35:03 +0900

    add rspec

--- Gemfile
+++ Gemfile
@@ -7,4 +7,8 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'webrick'
-gem 'rack-test'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'rack/test'
+require 'rspec'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+ENV['RACK_ENV'] = 'test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.include RSpecMixin
+end
