0bd73755e20f0185bdd551baad92adca881bf0c7
Aso2001026 <2001026@s.asojuku.ac.jp>
2021-08-21 13:55:08 +0900

    /Aso2001026エンドポイントを追加した

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require 'Aso2001026'
+require 'Aso2001026/reloader'
 
 get '/' do
   'Hello world!'
+end
+
+
+get '/Aso2001026' do
+  'Hello challecara!'
 end
